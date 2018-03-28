4e9b5c95e0f2af8c382000fad2781753e916c739
Tom Kopp <witzbould@live.com>
2018-03-28 12:49:03 +0200

    set system brightness via intent

--- app/src/main/java/de/tud/loomospeech/IntentsLibrary.java
+++ app/src/main/java/de/tud/loomospeech/IntentsLibrary.java
@@ -1,11 +1,18 @@
 package de.tud.loomospeech;
 
+import android.provider.Settings;
 import android.util.Log;
+import android.view.WindowManager;
 
 import org.json.JSONArray;
 
 class IntentsLibrary {
     private static final String TAG = "IntentsLibrary";
+    private MainActivity activity;
+
+    IntentsLibrary(MainActivity myActivity) {
+        activity = myActivity;
+    }
 
     public void callByName(String functionName, JSONArray entities) {
         //Ignoring any possible result
@@ -35,6 +42,15 @@ class IntentsLibrary {
     private void UtilitiesStop() {}
 
     private void OnDeviceSetBrightness (JSONArray entities) {
+        int brightness = 50;
 
+        //Set the system brightness using the brightness variable value
+        Settings.System.putInt(activity.cResolver, Settings.System.SCREEN_BRIGHTNESS, brightness);
+        //Get the current window attributes
+        WindowManager.LayoutParams layoutpars = activity.window.getAttributes();
+        //Set the brightness of this window
+        layoutpars.screenBrightness = brightness / (float)255;
+        //Apply attribute changes to this window
+        activity.window.setAttributes(layoutpars);
     }
 }

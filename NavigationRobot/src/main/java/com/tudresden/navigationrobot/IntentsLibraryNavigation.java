9f396881ea4f6ae9f3f58bdabf3fbdcbc5861c22
pcosel <casymoo@web.de>
2018-06-12 17:53:56 +0100

    added MovementMove to IntentsLibrary

--- NavigationRobot/src/main/java/com/tudresden/navigationrobot/IntentsLibraryNavigation.java
+++ NavigationRobot/src/main/java/com/tudresden/navigationrobot/IntentsLibraryNavigation.java
@@ -1,5 +1,10 @@
 package com.tudresden.navigationrobot;
 
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 public class IntentsLibraryNavigation extends de.tud.loomospeech.IntentsLibrary {
 
     protected MainActivity activity;
@@ -28,5 +33,97 @@ public class IntentsLibraryNavigation extends de.tud.loomospeech.IntentsLibrary 
                 activity.loomoRecognizer.startWakeUpListener();
             }
         });
+    }
+
+    protected void MovementMove (JSONArray entities) {
+        if (entities.length() > 0) {
+            try {
+                String directionString = "";
+                String degreesString = "";
+                String distanceString = "";
+                for (int i = 0; i < entities.length(); i++) {
+                    JSONObject entity = entities.getJSONObject(i);
+                    String value = "";
+                    if (entity.get("type").toString().equals("Movement.Direction") && directionString == "") {
+                        directionString = entity.get("entity").toString();
+                    }
+
+                    if (entity.get("type").toString().equals("Movement.Distance") && distanceString == "") {
+                        distanceString = entity.get("entity").toString();
+                    }
+
+                    if (entity.get("type").toString().equals("Movement.Degrees") && degreesString == "") {
+                        degreesString = entity.get("entity").toString();
+                    }
+                }
+
+                if(degreesString != "") {
+                    float degrees = Float.parseFloat(degreesString);
+                    switch (directionString) {
+                        case "right":
+                            activity.getExploration().turn(-degrees);
+                            break;
+                        case "left": case "":
+                            activity.getExploration().turn(degrees);
+                            break;
+                        default:
+                            Speak("I can't do that.", "Movement.Move", new Runnable() {
+                                @Override
+                                public void run() {
+                                    activity.loomoRecognizer.startWakeUpListener();
+                                }
+                            });
+                    }
+                } else if(distanceString == "") {
+                    switch(directionString) {
+                        case "right":
+                            activity.getExploration().turn(-90);
+                            break;
+                        case "left":
+                            activity.getExploration().turn(90);
+                            break;
+                        case "forward":case "forwards":
+                            activity.getExploration().move(1);
+                            break;
+                        case "backward":case "backwards": case "back":
+                            activity.getExploration().move(-1);
+                            break;
+                        default:
+                            Speak("I can't do that.", "Movement.Move", new Runnable() {
+                                @Override
+                                public void run() {
+                                    activity.loomoRecognizer.startWakeUpListener();
+                                }
+                            });
+                    }
+                } else {
+                    float distance = Float.parseFloat(distanceString);
+                    switch(directionString) {
+                        case "": case "forward":case "forwards":
+                            activity.getExploration().move(distance);
+                            break;
+                        case "backward":case "backwards": case "back":
+                            activity.getExploration().move(-distance);
+                            break;
+                        default:
+                            Speak("I can't do that.", "Movement.Move", new Runnable() {
+                                @Override
+                                public void run() {
+                                    activity.loomoRecognizer.startWakeUpListener();
+                                }
+                            });
+                    }
+                }
+            } catch (Exception e) {
+                Log.d(TAG, "Exception: ", e);
+            }
+        } else {
+           Speak("I can't do that.", "Movement.Move", new Runnable() {
+                @Override
+                public void run() {
+                    activity.loomoRecognizer.startWakeUpListener();
+                }
+            });
+        }
     }
 }
